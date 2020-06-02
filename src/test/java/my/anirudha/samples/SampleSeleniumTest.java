159158e6a76d9fbee700cf3ce337513df440c1fd
anirudha13 <anirudha.khanna@gmail.com>
2020-06-02 20:46:03 +0530

    Fixed unit test for running on remote hub

--- src/test/java/my/anirudha/samples/SampleSeleniumTest.java
+++ src/test/java/my/anirudha/samples/SampleSeleniumTest.java
@@ -1,11 +1,16 @@
 package my.anirudha.samples;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
@@ -13,6 +18,8 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 /**
  * Created with IntelliJ IDEA.
@@ -24,18 +31,56 @@ public class SampleSeleniumTest {
     private static final Logger LOGGER = LogManager.getLogger(SampleSeleniumTest.class);
     private static final String SCREENSHOT_FILE_PATH = "live-dashboard.png";
 
+    private static final String USERNAME = "anirudhakhanna5";
+    private static final String AUTOMATE_KEY = "yUGx7w2dij48Wfg6cRQb";
+    private static final String REMOTE_HUB_URL = "https://" + USERNAME + ":" + AUTOMATE_KEY + "@hub-cloud.browserstack.com/wd/hub";
+
     @Test
-    public void testBrowserStackLogin() throws Exception {
+    @Ignore
+    public void testBrowserStackLoginOnLocal() throws Exception {
         /* =================== Prepare ================= */
         System.setProperty("webdriver.chrome.driver", "/Users/anirudha/bin/chromedriver");
         WebDriver webDriver = new ChromeDriver();
 
         /* =================== Execute & Verify ================= */
+        runTest(webDriver);
+    }
+
+    @Test
+    public void testBrowserStackLoginOnHub() throws Exception {
+        /* =================== Prepare ================= */
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("browserName", "Chrome");
+        capabilities.setCapability("browserVersion", "83.0");
+
+        Map<String, Object> browserstackOptions = new HashMap<String, Object>();
+        browserstackOptions.put("os", "OS X");
+        browserstackOptions.put("osVersion", "Catalina");
+        browserstackOptions.put("projectName", "Onboarding Samples");
+        browserstackOptions.put("buildName", "JUnit-Selenium");
+        browserstackOptions.put("sessionName", "Browserstack-Login");
+        browserstackOptions.put("local", "false");
+        browserstackOptions.put("consoleLogs", "info");
+        browserstackOptions.put("networkLogs", "true");
+        capabilities.setCapability("bstack:options", browserstackOptions);
+
+
+        WebDriver webDriver = new RemoteWebDriver(new URL(REMOTE_HUB_URL), capabilities);
+
+        /* =================== Execute & Verify ================= */
+        runTest(webDriver);
+    }
+
+    private void runTest(WebDriver webDriver) throws IOException, InterruptedException {
         LOGGER.info("Navigating to BrowserStack home page");
         webDriver.get("https://www.browserstack.com/");
         WebElement signInElement = webDriver.findElement(By.linkText("Sign in"));
         Assert.assertNotNull("No Sign In button found", signInElement);
         signInElement.click();
+
+        LOGGER.info("Accepting Cookie Notifications");
+        WebElement acceptCookieButton = webDriver.findElement(By.id("accept-cookie-notification"));
+        acceptCookieButton.click();
 
         LOGGER.info("Starting Sign In actions");
         WebElement emailElement = webDriver.findElement(By.id("user_email_login"));
@@ -47,6 +92,7 @@ public class SampleSeleniumTest {
         passwordElement.sendKeys("Automate@123");
         Thread.sleep(100);
         submitElement.click();
+
         LOGGER.info("Successfully signed in to BrowserStack Live dashboard...waiting for dashboard to load");
 
         /* =================== Verify ================= */
@@ -62,6 +108,7 @@ public class SampleSeleniumTest {
         //Copy file at destination
         FileUtils.copyFile(srcFile, destFile);
 
+        LOGGER.info("Completed Live Dashboard test");
         webDriver.quit();
     }
 }
